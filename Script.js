a80870bd6a82f6205e03cabd6f20088bc251242e
ArthurVieira-eng <arthurv.o.2507@gmail.com>
2025-06-08 20:54:58 -0300

    ValidandoSenhas

---
+++ Script.js
@@ -0,0 +1,30 @@
+const { stdin, stdout } = require('process');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: stdin,
+    output: stdout
+}); 
+console.log("Digite uma senha que tenha pelo menos 8 caracteres, Letras Maiusculas e Minusculas, Número, Caractere especial.");
+rl.question('Digite sua senha aqui para verificar se é válida :', senha => {  
+    const LetraMaiuscula = /[A-Z]/.test(senha); 
+    const LetraMinuscula = /[a-z]/.test(senha);
+    const Numero = /\d/.test(senha);
+    const CaractersEspeciais = /[!@#$%&*<>:,.]/.test(senha);
+    const TamanhoPadrao = senha.length >= 8; 
+    if(LetraMaiuscula && LetraMinuscula && Numero && CaractersEspeciais && TamanhoPadrao) {
+        console.log("Senha Válida e Confirmada");
+    }else {
+        console.log("Senha Inválida. Veja o por que:");
+        if(!LetraMaiuscula)
+            console.log("Sua senha está faltando letras Maiusculas");
+        if(!LetraMinuscula)
+            console.log("Sua senha está faltando letras Minuscula");
+        if(!Numero)
+            console.log("Sua senha está faltando Números"); 
+        if(!CaractersEspeciais)
+            console.log("Sua senha está faltando caracteres especiais");
+        if(!TamanhoPadrao)
+            console.log("Sua senha precisa ter no mínimo 8 caracteres");
+    }
+    rl.close();
+}); 
