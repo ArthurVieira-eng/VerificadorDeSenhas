30031ed27c0b3b680219ab2262835b27e3028bf0
ArthurVieira-eng <arthurv.o.2507@gmail.com>
2025-07-31 23:19:59 -0300

    Update Script.js
    Implementação de comentários.

--- Script.js
+++ Script.js
@@ -1,16 +1,20 @@
+//Variáveis para interação do usuário com a interface.
 const { stdin, stdout } = require('process');
 const readline = require('readline');
 const rl = readline.createInterface({
     input: stdin,
     output: stdout
 }); 
-console.log("Digite uma senha que tenha pelo menos 8 caracteres, Letras Maiusculas e Minusculas, Número, Caractere especial.");
+//Primeira interação. 
+console.log("Digite uma senha que tenha pelo menos 8 caracteres, Letras Maiusculas e Minusculas, Número, Caractere especial."); 
+//Questionamento ao Usuário e ultilização de Arrow Function. 
 rl.question('Digite sua senha aqui para verificar se é válida :', senha => {  
     const LetraMaiuscula = /[A-Z]/.test(senha); 
     const LetraMinuscula = /[a-z]/.test(senha);
     const Numero = /\d/.test(senha);
     const CaractersEspeciais = /[!@#$%&*<>:,.]/.test(senha);
     const TamanhoPadrao = senha.length >= 8; 
+    //Verificação das variavéis. 
     if(LetraMaiuscula && LetraMinuscula && Numero && CaractersEspeciais && TamanhoPadrao) {
         console.log("Senha Válida e Confirmada");
     }else {
@@ -26,5 +30,6 @@ rl.question('Digite sua senha aqui para verificar se é válida :', senha => {
         if(!TamanhoPadrao)
             console.log("Sua senha precisa ter no mínimo 8 caracteres");
     }
+    //Fechando interface com usuário. 
     rl.close();
 }); 
